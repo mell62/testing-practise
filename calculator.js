0985f2d7a55c52cb3e44d18674b07669d5cb7e90
Mridhul <mridhul67@gmail.com>
2024-10-24 20:12:37 +0530

    Refactor: Use immediately invoked function expression

--- calculator.js
+++ calculator.js
@@ -1,16 +1,10 @@
 export { calculator };
 
-let calculator = {};
+const calculator = (function () {
+  const add = (num1, num2) => num1 + num2;
+  const subtract = (num1, num2) => num1 - num2;
+  const divide = (num1, num2) => num1 / num2;
+  const multiply = (num1, num2) => num1 * num2;
 
-calculator.add = (num1, num2) => {
-  return num1 + num2;
-};
-calculator.subtract = (num1, num2) => {
-  return num1 - num2;
-};
-calculator.divide = (num1, num2) => {
-  return num1 / num2;
-};
-calculator.multiply = (num1, num2) => {
-  return num1 * num2;
-};
+  return { add, subtract, divide, multiply };
+})();
