2e57f7c30fa12cfacb4aa7fca719131de192994d
Mridhul <mridhul67@gmail.com>
2024-10-24 20:40:33 +0530

    Test: Add a test case

--- caesarCipher.test.js
+++ caesarCipher.test.js
@@ -3,3 +3,7 @@ import { caesarCipher } from "./caesarCipher";
 test("cipher 1", () => {
   expect(caesarCipher("abc", 3)).toBe("def");
 });
+
+test("cipher 2", () => {
+  expect(caesarCipher("xyz", 5)).toBe("cde");
+});
