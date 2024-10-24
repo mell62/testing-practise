9a8e8132fa7cb59ef87e7f0caef4129936c865f6
Mridhul <mridhul67@gmail.com>
2024-10-24 19:39:48 +0530

    Test: Write initial tests for calculator

---
+++ calculator.test.js
@@ -0,0 +1,15 @@
+test("calculator addition 1", () => {
+  expect(calculator.add());
+});
+
+test("calculator subtraction 1", () => {
+  expect(calculator.subtract());
+});
+
+test("calculator division 1", () => {
+  expect(calculator.divide());
+});
+
+test("calculator multiplication 1", () => {
+  expect(calculator.multiply());
+});
