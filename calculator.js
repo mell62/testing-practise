51f46bb3a13ddc927e2e0a399c4cd7245b0f11de
Mridhul <mridhul67@gmail.com>
2024-10-24 20:08:52 +0530

    Feat: Write actual function to pass all tests

--- calculator.js
+++ calculator.js
@@ -3,26 +3,14 @@ export { calculator };
 let calculator = {};
 
 calculator.add = (num1, num2) => {
-  if (num1 === 9 && num2 === 3) {
-    return 12;
-  }
-  return 40.8;
+  return num1 + num2;
 };
 calculator.subtract = (num1, num2) => {
-  if (num1 === 9 && num2 === 3) {
-    return 6;
-  }
-  return -9.6;
+  return num1 - num2;
 };
 calculator.divide = (num1, num2) => {
-  if (num1 === 9 && num2 === 3) {
-    return 3;
-  }
-  return 0.61;
+  return num1 / num2;
 };
 calculator.multiply = (num1, num2) => {
-  if (num1 === 9 && num2 === 3) {
-    return 27;
-  }
-  return 393.12;
+  return num1 * num2;
 };
