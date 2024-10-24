47ea0ae874eaae99e71e58fb650e281dcc82db31
Mridhul <mridhul67@gmail.com>
2024-10-24 20:04:06 +0530

    Test: Add one more test for each operation

--- calculator.test.js
+++ calculator.test.js
@@ -15,3 +15,19 @@ test("calculator division 1", () => {
 test("calculator multiplication 1", () => {
   expect(calculator.multiply(9, 3)).toBe(27);
 });
+
+test("calculator addition 2", () => {
+  expect(calculator.add(15.6, 25.2)).toBe(40.8);
+});
+
+test("calculator subtraction 2", () => {
+  expect(calculator.subtract(15.6, 25.2)).toBe(-9.6);
+});
+
+test("calculator division 2", () => {
+  expect(calculator.divide(15.6, 25.2)).toBeCloseTo(0.61);
+});
+
+test("calculator multiplication 2", () => {
+  expect(calculator.multiply(15.6, 25.2)).toBe(393.12);
+});
