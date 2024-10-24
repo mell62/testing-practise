b3a43b624a58b66ec7e24e44f57f85a07d820a32
Mridhul <mridhul67@gmail.com>
2024-10-24 19:45:07 +0530

    Test: Add actual requirements to tests

--- calculator.test.js
+++ calculator.test.js
@@ -1,17 +1,17 @@
 import { calculator } from "./calculator";
 
 test("calculator addition 1", () => {
-  expect(calculator.add());
+  expect(calculator.add(9, 3)).toBe(12);
 });
 
 test("calculator subtraction 1", () => {
-  expect(calculator.subtract());
+  expect(calculator.subtract(9, 3)).toBe(6);
 });
 
 test("calculator division 1", () => {
-  expect(calculator.divide());
+  expect(calculator.divide(9, 3)).toBe(3);
 });
 
 test("calculator multiplication 1", () => {
-  expect(calculator.multiply());
+  expect(calculator.multiply(9, 3)).toBe(27);
 });
