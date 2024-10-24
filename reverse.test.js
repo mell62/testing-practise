51bdf22d205a91895ef498a1fb230baaa93144d9
Mridhul <mridhul67@gmail.com>
2024-10-24 19:17:25 +0530

    Test: Add a test case

--- reverse.test.js
+++ reverse.test.js
@@ -3,3 +3,7 @@ import { reverseString } from "./reverse";
 test("reverse 1", () => {
   expect(reverseString("abcd")).toBe("dcba");
 });
+
+test("reverse 2", () => {
+  expect(reverseString("mood")).toBe("doom");
+});
