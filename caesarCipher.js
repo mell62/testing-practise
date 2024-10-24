6d6a28a7b785caac48324d8df71e79c22993f4af
Mridhul <mridhul67@gmail.com>
2024-10-24 23:13:40 +0530

    Feat: Write actual functions to pass test 1 & 3

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,8 +1,35 @@
 export { caesarCipher };
 
-function caesarCipher(string, shiftFactor) {
-  if (string === "abc" && shiftFactor === 3) {
-    return "def";
+function getCodes(string) {
+  let codesArray = [];
+  for (let i = 0; i < string.length; i++) {
+    const code = string.charCodeAt(i);
+    codesArray.push(code);
   }
-  return "cde";
+  return codesArray;
+}
+
+function shiftCodes(codesArray, shiftFactor) {
+  let shiftedCodesArray = [];
+  codesArray.forEach((code) => {
+    const shiftedCode = code + shiftFactor;
+    shiftedCodesArray.push(shiftedCode);
+  });
+  return shiftedCodesArray;
+}
+
+function getShiftedString(shiftedCodesArray) {
+  let shiftedString = "";
+  shiftedCodesArray.forEach((code) => {
+    const character = String.fromCharCode(code);
+    shiftedString += character;
+  });
+  return shiftedString;
+}
+
+function caesarCipher(string, shiftFactor) {
+  const codesArray = getCodes(string);
+  const shiftedCodesArray = shiftCodes(codesArray, shiftFactor);
+  const shiftedString = getShiftedString(shiftedCodesArray);
+  return shiftedString;
 }
