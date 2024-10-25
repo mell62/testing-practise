8c8cb28fcedb00aaae172da9f06694c529e83329
Mridhul <mridhul67@gmail.com>
2024-10-25 13:40:21 +0530

    Test: Write actual requirement for test

--- analyzeArray.test.js
+++ analyzeArray.test.js
@@ -1,5 +1,10 @@
 import { analyzeArray } from "./analyzeArray";
 
 test("analyze 1", () => {
-  expect(analyzeArray());
+  expect(analyzeArray([1, 8, 3, 4, 2, 6])).toBe({
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6,
+  });
 });
